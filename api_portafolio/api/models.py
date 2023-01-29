5c51fc4bd779e0de40d1e33ad1c962cbdf516ab0
hscordonez1810 <hsc.ordonez@yavirac.edu.ec>
2023-01-28 23:11:26 -0500

    feat:db portafolio migracion

--- api_portafolio/api/models.py
+++ api_portafolio/api/models.py
@@ -3,11 +3,45 @@ from django.db import models
 class Solicitud (models.Model):
     estado= models.BooleanField()
     tipo_solicitud = models.CharField(max_length=15)
-    fecha_emision =  models.DateField(auto_now=False,auto_now_add=False)
     
+class Catalogo (models.Model):
+    rol_docente_vinculacion = models.IntegerField()
+    rol_docente_tutor = models.IntegerField()
+    rol_estudiante = models.IntegerField()
+    rol_coordinador_general = models.IntegerField()
+    rol_voordinador_carrera = models.IntegerField()
+
 class Persona(models.Model):
     nombre = models.CharField(max_length=60)
     cedula = models.CharField(max_length=10)
     telefono = models.CharField(max_length=10)
     direccion = models.CharField(max_length=70)
+    
+class User (models.Model):
+    usuario = models.CharField(max_length=20)
+    clave = models.CharField(max_length=20)
+
+class TipoPersonaCatalogo (models.Model):
+    semestre = models.CharField(max_length=15)
+    carrera = models.CharField(max_length=25)    
+    jornada = models.CharField(max_length=10)
+    catalogo = models.ForeignKey(Catalogo, on_delete=models.PROTECT)
+    idPersona = models.ForeignKey(Persona, on_delete=models.PROTECT)
+    
+class Portafolio (models.Model):
+    nombre_archivo = models.CharField(max_length=60)
+    opciones = models.CharField(max_length=10)
+    observaciones = models.CharField(max_length=10)
+    aprobar = models.BooleanField(default=False)
+    dirigido = models.BooleanField(default=False)
+    estado = models.BooleanField(default=False)
+    idTipoPersona = models.ForeignKey(TipoPersonaCatalogo, on_delete=models.PROTECT)
+
+class Fundacion (models.Model):
+    nombre = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=70)
+    encargado = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=10)
+    estado = models.BooleanField(default=False)
+    idTipoPersona = models.ForeignKey(TipoPersonaCatalogo, on_delete=models.PROTECT)
 # Create your models here.
--- api_portafolio/api/views.py
+++ api_portafolio/api/views.py
@@ -1,15 +1,34 @@
+from api.models import Catalogo, TipoPersonaCatalogo, Persona, Portafolio, Fundacion, User, Solicitud
+from api.serializers import CatalogoSerializer, TipoPersonaCatalogoSerializer, PersonaSerializer, FundacionSerializer, UserSerializer, PortafolioSerializer, SolicitudSerializer
 from rest_framework import viewsets
 
-from api.serializers import SolicitudSerializer,PersonaSerializer
-from api.models import Solicitud,Persona
+
 
 class SolicitudViewSet (viewsets.ModelViewSet):
     serializer_class = SolicitudSerializer
     queryset = Solicitud.objects.all()
-    
 
 class PersonaViewSet (viewsets.ModelViewSet):
     serializer_class = PersonaSerializer
     queryset = Persona.objects.all()
 
+class CatalogoViewSet (viewsets.ModelViewSet):
+    serializer_class = CatalogoSerializer
+    queryset = Catalogo.objects.all()
+
+class CatalogoViewSet (viewsets.ModelViewSet):
+    serializer_class = TipoPersonaCatalogoSerializer
+    queryset = TipoPersonaCatalogo.objects.all()
+
+class PortafolioViewSet (viewsets.ModelViewSet):
+    serializer_class = PortafolioSerializer
+    queryset = Portafolio.objects.all()
+    
+class FundacionViewSet (viewsets.ModelViewSet):
+    serializer_class = FundacionSerializer
+    queryset = Fundacion.objects.all()
+    
+class UserViewSet (viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 # Create your views here.
