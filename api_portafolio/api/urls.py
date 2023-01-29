e817c660f656fe4da26151a0612b86a40e39f709
hscordonez1810 <hsc.ordonez@yavirac.edu.ec>
2023-01-29 18:26:18 -0500

    feat:urls

--- api_portafolio/api/urls.py
+++ api_portafolio/api/urls.py
@@ -1,14 +1,15 @@
 from rest_framework.routers import DefaultRouter
-from api.views import SolicitudViewSet,PersonaViewSet,TipoPersonaCatalogoViewSet,CatalogoViewSet
+from api.views import SolicitudViewSet,PersonaViewSet,TipoPersonaCatalogoViewSet,CatalogoViewSet,FundacionSerializer,PortafolioSerializer,UserSerializer
 
 router = DefaultRouter()
 
 router.register('api/solicitud', SolicitudViewSet)
-
+router.register('api/fundacion', FundacionSerializer)
 router.register('api/persona', PersonaViewSet)
-
 router.register('api/tipo-persona',TipoPersonaCatalogoViewSet )
 router.register('api/catalogo',CatalogoViewSet )
+router.register('api/portafolio',PortafolioSerializer )
+router.register('api/users',UserSerializer )
 
 urlpatterns = []
 
